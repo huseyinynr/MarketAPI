638c34a12f37a1dbe436a75f62fd95da37df0dc2
Bekir Mese <Bekirmese07@hotmail.com>
2026-06-12 15:14:30 +0300

    Enable EnsureCreated for automatic database initialization with seeds

--- MarketAPI/Program.cs
+++ MarketAPI/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
