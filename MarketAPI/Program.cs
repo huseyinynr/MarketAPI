7bdbe5186f9e01a720ba19d50ba951da8fd2700e
Bekir Mese <Bekirmese07@hotmail.com>
2026-06-28 15:56:52 +0300

    Auto reset DB on every startup so pushed market.db always takes effect

--- MarketAPI/Program.cs
+++ MarketAPI/Program.cs
@@ -27,6 +27,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
     AppDbContext.InitializeAsync(db).Wait();
 }
 
